7fba6360009cda7db98c5b6064bd682ea83e9302
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2018-08-15 15:40:12 +0530

    Initial commit

---
+++ src/main/java/com/boot/config/PresistenceConfiguration.java
@@ -0,0 +1,56 @@
+package com.boot.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PresistenceConfiguration {
+
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    @Primary
+//    public DataSource dataSource(){
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "datasource.flyway")
+//    @FlywayDataSource
+//    public DataSource flywayDataSource(DataSourceProperties properties){
+//        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).create().build();
+//    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties firstDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource firstDataSource() {
+        return firstDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("datasource.flyway")
+    @FlywayDataSource
+    public DataSourceProperties secondDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("datasource.flyway")
+    @FlywayDataSource
+    public DataSource secondDataSource() {
+        return secondDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+}
